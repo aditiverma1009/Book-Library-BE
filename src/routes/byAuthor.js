ee7e9b9928f909b7964d365b2814bad2e3488a49
aditiverma1009 <aditiverma1009@gmail.com>
2018-02-24 14:03:37 +0530

    By author route working

--- src/routes/byAuthor.js
+++ src/routes/byAuthor.js
@@ -3,12 +3,14 @@ const fetch = require('../../src/fetch');
 
 const getBooksFromAPI1 = (request, response) => {
   fetch().then((books) => {
+    // console.log(books);
     const booksByAuthor = {};
     books.forEach((book) => {
-      if (book.Author in booksByAuthor) {
-        booksByAuthor[book.Author].push(book);
+      if (book.author in booksByAuthor) {
+        // console.log(`if not exist${book.author}`);
+        booksByAuthor[book.author].push(book);
       } else {
-        const author = book.Author;
+        const { author } = book;
         booksByAuthor[author] = [];
         booksByAuthor[author].push(book);
       }
